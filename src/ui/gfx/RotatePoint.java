73b51a7cc73fe8e30bd67a14eb9594af1bb0e13c
Daniel <wilkowski.kontakt@gmail.com>
2017-10-14 16:14:49 +0200

    Make DrawFrom immutable

--- src/ui/gfx/RotatePoint.java
+++
@@ -1,9 +0,0 @@
-package ui.gfx;
-
-class RotatePoint extends RatioPoint {
-    private RotatePoint(double x, double y) {
-        super(x, y);
-    }
-
-    static RotatePoint Center = new RotatePoint(0.0, 0.0);
-}
