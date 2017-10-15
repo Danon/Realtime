fa7743b2da65fbdfac53a825009d1d060b4f4175
Daniel <wilkowski.kontakt@gmail.com>
2017-10-15 22:46:40 +0200

    Overlapping with obstacle won't hide your whole field of view

--- src/ui/gfx/shadows/MasterOfShadows.java
+++ src/ui/gfx/shadows/MasterOfShadows.java
@@ -6,7 +6,9 @@ import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static ui.gfx.shadows.ShadowGeometry.Intersection.intersectLineLine;
 import static ui.gfx.shadows.ShadowGeometry.Points.byAngleComparator;
@@ -14,6 +16,8 @@ import static ui.gfx.shadows.ShadowGeometry.Shapes.computeLineSegments;
 
 public class MasterOfShadows {
     private final List<List<Line2D>> lineSegments = new ArrayList<>();
+
+    private final Map<List<Line2D>, Shape> skip = new HashMap<>();
 
     public MasterOfShadows(int x, int y, int width, int height) {
         addLineSegment(makeBounds(x, y, width, height));
@@ -31,7 +35,9 @@ public class MasterOfShadows {
     }
 
     public void addObstacle(Shape shape) {
-        this.addLineSegment(computeLineSegments(shape, 1.0));
+        List<Line2D> lineSegments = computeLineSegments(shape, 1.0);
+        this.addLineSegment(lineSegments);
+        skip.put(lineSegments, shape);
     }
 
     private void addLineSegment(List<Line2D> lineSegments) {
@@ -41,7 +47,7 @@ public class MasterOfShadows {
     public void getFieldOfView(gameplay.Point light, IntersectionIterable iterable) {
         java.awt.Point pos = new java.awt.Point(light.getX(), light.getY());
 
-        List<Point2D> closestIntersections = this.computeClosestIntersections(this.createRays(pos));
+        List<Point2D> closestIntersections = this.computeClosestIntersections(this.createRays(pos), pos);
 
         closestIntersections.sort(byAngleComparator(pos));
         iterateLightShape(closestIntersections, iterable);
@@ -51,6 +57,8 @@ public class MasterOfShadows {
         final double deltaRad = 0.0001;
         List<Line2D> rays = new ArrayList<>();
         for (List<Line2D> shapeLineSegments : lineSegments) {
+            if (shouldSkip(lightPosition, shapeLineSegments)) continue;
+
             for (Line2D line : shapeLineSegments) {
                 Line2D ray0 = new Line2D.Double(lightPosition, line.getP1());
                 Line2D ray1 = new Line2D.Double(lightPosition, line.getP2());
@@ -66,10 +74,15 @@ public class MasterOfShadows {
         return rays;
     }
 
-    private List<Point2D> computeClosestIntersections(List<Line2D> rays) {
+    private boolean shouldSkip(Point2D lightPosition, List<Line2D> shapeLineSegments) {
+        Shape s = skip.get(shapeLineSegments);
+        return s != null && s.contains(lightPosition);
+    }
+
+    private List<Point2D> computeClosestIntersections(List<Line2D> rays, Point2D lightPosition) {
         List<Point2D> intersections = new ArrayList<>();
         for (Line2D ray : rays) {
-            Point2D closestIntersection = computeClosestIntersection(ray);
+            Point2D closestIntersection = computeClosestIntersection(ray, lightPosition);
             if (closestIntersection != null) {
                 intersections.add(closestIntersection);
             }
@@ -77,7 +90,7 @@ public class MasterOfShadows {
         return intersections;
     }
 
-    private Point2D computeClosestIntersection(Line2D ray) {
+    private Point2D computeClosestIntersection(Line2D ray, Point2D lightPosition) {
         final double EPSILON = 1e-6;
         Point2D relativeLocation = new Point2D.Double();
         Point2D absoluteLocation = new Point2D.Double();
@@ -85,6 +98,8 @@ public class MasterOfShadows {
         double minRelativeDistance = Double.MAX_VALUE;
 
         for (List<Line2D> lineSegments : this.lineSegments) {
+            if (shouldSkip(lightPosition, lineSegments)) continue;
+
             for (Line2D lineSegment : lineSegments) {
                 if (intersectLineLine(ray, lineSegment, relativeLocation, absoluteLocation)) {
                     if (relativeLocation.getY() >= -EPSILON && relativeLocation.getY() <= 1 + EPSILON) {
