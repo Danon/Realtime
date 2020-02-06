04c8c06c629e707f28eec678424ad34948ec2471
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-02-07 00:23:04 +0100

    Clean Camera

--- src/main/kotlin/ui/gfx/Camera.kt
+++ src/main/kotlin/ui/gfx/Camera.kt
@@ -2,6 +2,7 @@ package ui.gfx
 
 import gameplay.Point
 import gameplay.geometry.Rectangle
+import ui.gfx.Renderer.*
 
 class Camera(private var point: Point) {
 
@@ -10,8 +11,8 @@ class Camera(private var point: Point) {
 
     fun cap(borders: Rectangle, windowWidth: Int, windowHeight: Int) {
         point = Point(
-                point.capX(-Renderer.CAMERA_SIDE_MARGIN.toDouble(), borders.width - windowWidth + Renderer.CAMERA_SIDE_MARGIN),
-                point.capY(-Renderer.CAMERA_BOTTOM_MARGIN.toDouble(), borders.height - windowHeight + Renderer.CAMERA_TOP_MARGIN)
+                point.capX(-CAMERA_SIDE_MARGIN.toDouble(), borders.width - windowWidth + CAMERA_SIDE_MARGIN),
+                point.capY(-CAMERA_BOTTOM_MARGIN.toDouble(), borders.height - windowHeight + CAMERA_TOP_MARGIN)
         )
     }
 
