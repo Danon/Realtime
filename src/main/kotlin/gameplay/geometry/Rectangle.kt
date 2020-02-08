4cba1e307a27e471a7925be40e6cb45c9df7f8f1
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-02-08 13:54:56 +0100

    Rectangle.kt

--- src/main/kotlin/gameplay/geometry/Rectangle.kt
+++ src/main/kotlin/gameplay/geometry/Rectangle.kt
@@ -10,12 +10,6 @@ class Rectangle(val x: Double,
     constructor(p: Point, width: Double, height: Double) : this(p.x, p.y, width, height)
     constructor(r: Rectangle) : this(r.x, r.y, r.width, r.height)
 
-    val intWidth: Int
-        get() = width.toInt()
-
-    val intHeight: Int
-        get() = height.toInt()
-
     val intX: Int
         get() = x.toInt()
 
